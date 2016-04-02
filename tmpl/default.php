1883d7142c05e232d18d40d21c7758dad95ac9ef
Thomas.Finnern <GitProjects@insidethemachine.de>
2016-04-02 21:15:58 +0200

    Added path

--- tmpl/default.php
+++ tmpl/default.php
@@ -38,12 +38,12 @@ defined('_JEXEC') or die();
 				if ($displayType == 1) {
 					// *** display ***: 
 					$watermark = $rsgConfig->get('watermark');
-					$imageUrl = $watermark ? waterMarker::showMarkedImage( $ItemIdxName ) : imgUtils::getImgDisplay( $ItemIdxName );
+					$imageUrl = $watermark ? waterMarker::showMarkedImage( $ItemIdxName ) : imgUtils::getImgDisplayPath( $ItemIdxName );
 					$HTML = '<img class="rsg2-displayImage" src="'.$imageUrl.'" alt="'.$ItemIdxName.'" title="'.$ItemIdxName.'" '.$imgAttributes.'/>';
 				} elseif ($displayType == 2) {
 					// *** original ***
 					$watermark = $rsgConfig->get('watermark');
-					$imageOriginalUrl = $watermark ? waterMarker::showMarkedImage( $ItemIdxName, 'original' ) : imgUtils::getImgOriginal( $ItemIdxName );
+					$imageOriginalUrl = $watermark ? waterMarker::showMarkedImage( $ItemIdxName, 'original' ) : imgUtils::getImgOriginalPath( $ItemIdxName );
 					$HTML = '<img class="rsg2-displayImage" src="'.$imageOriginalUrl.'" alt="'.$ItemIdxName.'" title="'.$ItemIdxName.'" '.$imgAttributes.'/>';
 				} else {
 					// *** thumb ***
